5a5319a97b2a2ca707fb5447793d43bb9298948f
hossein <hossein.h.a770227@gmail.com>
2025-05-19 16:16:37 +0330

    add product admin

--- mobile/admin.py
+++ mobile/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductImageModel
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'inventory', 'price_main','created_date']
+    search_fields = ['title', 'inventory', 'price_main']
+    list_per_page = 10
+    list_filter = ['title', ]
+    prepopulated_fields = {
+        'slug':['title']
+    }
